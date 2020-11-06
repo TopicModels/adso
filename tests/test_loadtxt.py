a7ee82c4598a1069c4fb2b1db074b966fb36dd00
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2020-11-06 18:04:57 +0100

    switched to torch

--- tests/test_loadtxt.py
+++ tests/test_loadtxt.py
@@ -1,8 +1,10 @@
 """Test load_txt function from data module."""
 
-from adso.data.Test import (_write_test_dataset, load_labelled_test_dataset,
-                            load_test_dataset)
-import tensorflow as tf
+from adso.data.test import (
+    _write_test_dataset,
+    load_labelled_test_dataset,
+    load_test_dataset,
+)
 
 
 def test_write_dir():
@@ -10,22 +12,14 @@ def test_write_dir():
 
 
 def test_load():
-    assert load_test_dataset(lines=True).get_data().shape == (12,)
-    assert load_labelled_test_dataset(lines=True).get_data().shape == (12,)
-    assert load_labelled_test_dataset(lines=True).get_label().shape == (12,)
-    assert tf.reduce_all(
-        tf.equal(
-            tf.unique(load_labelled_test_dataset(lines=True).get_label())[0],
-            tf.convert_to_tensor(["linearalgebra", "geometry", "dinosaur", "bird"]),
-        )
+    assert len(load_test_dataset(lines=True).get_data()) == 12
+    assert len(load_labelled_test_dataset(lines=True).get_data()) == 12
+    assert len(load_labelled_test_dataset(lines=True).get_label()) == 12
+    assert set(load_labelled_test_dataset(lines=True).get_label()) == set(
+        ["linearalgebra", "geometry", "dinosaur", "bird"]
     )
 
-    assert load_test_dataset().get_data().shape == (4,)
-    assert load_labelled_test_dataset().get_data().shape == (4,)
-    assert load_labelled_test_dataset().get_label().shape == (4,)
-    assert tf.reduce_all(
-        tf.equal(
-            tf.unique(load_labelled_test_dataset().get_label())[0],
-            tf.convert_to_tensor(["maths", "animals"]),
-        )
-    )
+    assert len(load_test_dataset().get_data()) == 4
+    assert len(load_labelled_test_dataset().get_data()) == 4
+    assert len(load_labelled_test_dataset().get_label()) == 4
+    assert set(load_labelled_test_dataset().get_label()) == set(["maths", "animals"])
