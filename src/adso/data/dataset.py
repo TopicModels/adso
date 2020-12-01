c6805e55273219aee78fdfacce6eaa818079d516
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2020-12-01 23:40:48 +0100

    implemented + for dataset (concat)

--- src/adso/data/dataset.py
+++ src/adso/data/dataset.py
@@ -5,7 +5,7 @@ from typing import List, Tuple
 
 class Dataset:
     def __init__(self: Dataset, data: List[str]) -> None:
-        # add a chack type on data elements?
+        # add a check type on data elements?
         self.data: List(str) = data
 
     def get_data(self: Dataset) -> List[str]:
@@ -17,16 +17,25 @@ class Dataset:
     def __getitem__(self: Dataset, index: int):
         return self.data[index]
 
+    def __add__(self: Dataset, other: Dataset):
+        return Dataset(self.get_data() + other.get_data())
+
 
 class LabelledDataset(Dataset):
     def __init__(self: LabelledDataset, data: List[Tuple[str, str]]) -> None:
-        self.data, self.label = zip(*data)
+        self.data, self.labels = zip(*data)
 
     def get_data(self: LabelledDataset) -> List[str]:
         return self.data
 
-    def get_label(self: LabelledDataset) -> List[str]:
-        return self.label
+    def get_labels(self: LabelledDataset) -> List[str]:
+        return self.labels
 
     def __getitem__(self: Dataset, index: int):
-        return self.data[index], self.label[index]
+        return self.data[index], self.labels[index]
+
+    def __add__(self: LabelledDataset, other: LabelledDataset):
+        return LabelledDataset(
+            list(zip(self.get_data(), self.get_labels()))
+            + list(zip(other.get_data(), other.get_labels()))
+        )
