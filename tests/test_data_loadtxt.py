362dbb392c289fe5ae97f38b1bca7b65dcda3d96
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2020-12-01 23:43:00 +0100

    renamed

---
+++ tests/test_data_loadtxt.py
@@ -0,0 +1,25 @@
+"""Test load_txt function from data module."""
+
+from adso.data.test import (
+    _write_test_dataset,
+    load_labelled_test_dataset,
+    load_test_dataset,
+)
+
+
+def test_write_dir():
+    _write_test_dataset()
+
+
+def test_load():
+    assert len(load_test_dataset(lines=True).get_data()) == 12
+    assert len(load_labelled_test_dataset(lines=True).get_data()) == 12
+    assert len(load_labelled_test_dataset(lines=True).get_labels()) == 12
+    assert set(load_labelled_test_dataset(lines=True).get_labels()) == set(
+        "linearalgebra", "geometry", "dinosaur", "bird"
+    )
+
+    assert len(load_test_dataset().get_data()) == 4
+    assert len(load_labelled_test_dataset().get_data()) == 4
+    assert len(load_labelled_test_dataset().get_labels()) == 4
+    assert set(load_labelled_test_dataset().get_labels()) == set("maths", "animals")
