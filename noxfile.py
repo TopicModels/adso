c4acc792aa4b216b93157d8e2b45164ba102f4d9
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2021-04-03 23:08:51 +0200

    switch to conda

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,3 @@
-import tempfile
-
 import nox
 
 locations = "src/", "tests/", "noxfile.py", "docs/_source/conf.py"
@@ -8,45 +6,25 @@ nox.options.sessions = "black", "lint", "mypy"
 
 ADSODIR = ".adso_test"
 
+py_version = ["3.8", "3.9", "3.7", "3.6"]
+
 
 def install_this(session):
-    with tempfile.NamedTemporaryFile() as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--format=requirements.txt",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        session.install("-r", requirements.name)
-        session.install(".")
-
-
-def install_with_constraints(session, *args, **kwargs):
-    with tempfile.NamedTemporaryFile() as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--format=requirements.txt",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+    session.run("conda", "env", "update", "--file", "environment.yml")
+    session.install(".")
 
 
 @nox.session()
 def black(session):
     args = session.posargs or locations
-    install_with_constraints(session, "black")
+    session.install("black")
     session.run("black", *args)
 
 
 @nox.session()
 def lint(session):
     args = session.posargs or locations
-    install_with_constraints(
-        session,
+    session.install(
         "flake8",
         "flake8-annotations",
         "flake8-bandit",
@@ -62,22 +40,22 @@ def lint(session):
 @nox.session()
 def mypy(session):
     args = session.posargs or locations
-    install_with_constraints(session, "mypy")
+    session.install(mypy)
     session.run("mypy", *args)
 
 
-@nox.session(python=["3.9", "3.8", "3.7"])
+@nox.session(python=py_version, venv_backend="conda")
 def xdoctest(session):
     args = session.posargs or ["all"]
     install_this(session)
-    install_with_constraints(session, "xdoctest")
+    session.conda_install("xdoctest")
     session.run("python", "-m", "xdoctest", "adso", *args)
 
 
-@nox.session(python="3.8")
+@nox.session(python=py_version[0], venv_backend="conda")
 def cov(session):
     install_this(session)
-    install_with_constraints(session, "coverage[toml]", "pytest", "pytest-cov")
+    session.conda_install("coverage[toml]", "pytest", "pytest-cov")
     session.run("rm", "-rf", ADSODIR, external=True)
     session.run(
         "pytest",
@@ -91,20 +69,28 @@ def cov(session):
     )
 
 
-@nox.session(python=["3.9", "3.8", "3.7", "3.6"])
+@nox.session(python=py_version, venv_backend="conda")
 def test(session):
     install_this(session)
-    install_with_constraints(session, "pytest")
+    session.conda_install("pytest")
     session.run("rm", "-rf", ADSODIR, external=True)
     session.run("pytest", env={"ADSODIR": ADSODIR, "PYTHONWARNINGS": "default"})
 
 
-@nox.session(python="3.8")
+@nox.session(python=py_version)
+def poetry_test(session):
+    session.install("poetry", "pytest")
+    session.run("rm", "-rf", "poetry.lock", external=True)
+    session.run("poetry", "install")
+    session.install(".")
+    session.run("rm", "-rf", ADSODIR, external=True)
+    session.run("pytest", env={"ADSODIR": ADSODIR, "PYTHONWARNINGS": "default"})
+
+
+@nox.session(python=py_version[0], venv_backend="conda")
 def docs(session):
     install_this(session)
-    install_with_constraints(
-        session, "sphinx", "sphinx-autodoc-typehints", "sphinx-gallery"
-    )
+    session.conda_install("sphinx", "sphinx-autodoc-typehints", "sphinx-gallery")
     session.run("sphinx-build", "-b", "html", "docs/_source", "docs")
     session.run("sphinx-build", "-M", "latexpdf", "docs/_source", "docs/_latex")
     session.run("cp", "docs/_latex/latex/adso.pdf", "docs", external=True)
