ce1065c4f4e677fef8280f6d34148ac4af794a34
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2021-04-19 15:23:20 +0200

    recover 3.6 compatibility

--- src/adso/data/__init__.py
+++ src/adso/data/__init__.py
@@ -1,8 +1,5 @@
 """Datasets and datased related functions."""
 
-from __future__ import annotations
-
-from .common import DATADIR
 from .dataset import Dataset, LabeledDataset
 
 # Create data folder if it not exists
