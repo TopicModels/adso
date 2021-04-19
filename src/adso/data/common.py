9d85935a99f2e626b453ebade3ceec71616787e3
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2021-04-19 15:23:55 +0200

    nltk downloader and useful functions

--- src/adso/data/common.py
+++ src/adso/data/common.py
@@ -2,6 +2,9 @@
 
 import hashlib
 from pathlib import Path
+from typing import Iterable
+
+import nltk
 
 from .. import common as adso_common
 
@@ -23,3 +26,30 @@ def hash(path: Path) -> str:
                 break
             md5.update(data)
     return md5.hexdigest()
+
+
+def nltk_download(id: str) -> None:
+    """Wrapper for :func:`nltk.downloader.download`, to download nltk data inside
+    adso directory.
+
+    Args:
+        id (str): id of the required downloadable data. Cfr
+            <http://www.nltk.org/nltk_data/>
+    """
+    NLTKDIR = DATADIR / "nltk"
+    NLTKDIR.mkdir(exist_ok=True, parents=True)
+    if NLTKDIR not in nltk.data.path:
+        nltk.data.path.append(NLTKDIR)
+    nltk.downloader.download(id, download_dir=NLTKDIR)
+
+
+def tokenize_and_stem(doc: str) -> Iterable[str]:
+    nltk_download("punkt")
+    tokenizer = nltk.tokenize.word_tokenize
+    stemmer = nltk.stem.SnowballStemmer("english")
+    return map(stemmer.stem, tokenizer(doc))
+
+
+def get_nltk_stopwords() -> Iterable[str]:
+    nltk_download("stopwords")
+    return nltk.corpus.stopwords.words("english")
