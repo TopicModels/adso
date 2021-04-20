2ebd7542553c171f33e46607227c9cc56b1fcf36
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2021-04-20 11:05:42 +0200

    placeholder

--- src/adso/__init__.py
+++ src/adso/__init__.py
@@ -1,4 +1,4 @@
 """Adso is a topic-modelling library based on numpy and nltk."""
 
 from . import data
-from .common import set_adso_dir, set_project_name, set_seed
+from .common import set_adso_dir, set_project_name, set_seed, setup_dask_client
