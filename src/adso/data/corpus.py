b20b6464baa09975385f0193426fda8e05cee8a7
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2021-04-20 11:06:54 +0200

    rename hash function

--- src/adso/data/corpus.py
+++ src/adso/data/corpus.py
@@ -2,23 +2,26 @@
 
 from abc import ABC, abstractmethod
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, Union
 
 import dask.array as da
 import h5py
 
 
-from .common import hash
+from .common import compute_hash
 
 
 class Corpus(ABC):
     def __init__(self, path: Path) -> None:
         self.path = path
-        self.hash: str = hash(self.path)
+        self.update_hash()
 
     @abstractmethod
     def get(self) -> Any:
         raise NotImplementedError
+
+    def update_hash(self) -> None:
+        self.hash = compute_hash(self.path)
 
     def serialize(self) -> Dict[str, str]:
         return {
@@ -28,28 +31,29 @@ class Corpus(ABC):
         }
 
     @classmethod
-    def load(cls, path: Path, hash: str) -> "Corpus":
+    def load(cls, path: Union[Path, str], hash: str) -> "Corpus":
+        path = Path(path)
         if path.is_file():
             corpus = cls(path)
             if corpus.hash == hash:
                 return corpus
             else:
-                raise RuntimeError
+                raise RuntimeError("Different hash")
         else:
-            raise RuntimeError
+            raise RuntimeError("File already exists")
 
 
 class Raw(Corpus):
     def get(self) -> da.array:
-        if self.hash == hash(self.path):
+        if self.hash == compute_hash(self.path):
             return da.from_array(h5py.File(self.path, "r")["/raw"])
         else:
-            raise RuntimeError
+            raise RuntimeError("Different hash")
 
     @classmethod
     def from_dask_array(cls, path: Path, array: da.array) -> "Raw":
         if path.is_file():
-            raise RuntimeError
+            raise RuntimeError("File already exists")
         else:
             array.to_hdf5(path, "/raw", shuffle=False)
         return Raw(path)
--- src/adso/data/vectorizer.py
+++ src/adso/data/vectorizer.py
@@ -2,12 +2,13 @@ import pickle
 
 from dask_ml.feature_extraction.text import CountVectorizer
 
+from .common import compute_hash
 from .corpus import Corpus
 
 
 class Vectorizer(Corpus):
     def get(self) -> CountVectorizer:
-        if self.hash == hash(self.path):
+        if self.hash == compute_hash(self.path):
             return pickle.load(self.path.open("rb"))
         else:
-            raise RuntimeError
+            raise RuntimeError("Different hash")
