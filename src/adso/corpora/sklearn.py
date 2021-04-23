f2b692a5af7726522354d1d1c6ec3a48c5079e60
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2021-04-23 18:00:48 +0200

    20_newsgroup skeleton

--- src/adso/corpora/sklearn.py
+++ src/adso/corpora/sklearn.py
@@ -1,19 +1,28 @@
-import numpy as np
-import sklearn.dataset
+# encoding: utf-8
 
-from ..common import DATADIR, get_seed
+import numpy as np
+
+import sklearn.datasets
+
+from .. import common
+from ..common import get_seed
 from ..data import LabeledDataset
 
 
-def get_20newsgroups(name: str, **kwargs) -> LabeledDataset:
+def get_20newsgroups(name: str, overwrite: bool = False, **kwargs) -> LabeledDataset:
     # https://scikit-learn.org/stable/modules/generated/sklearn.datasets.fetch_20newsgroups.html
-    data, labels = sklearn.dataset.fetch_20newsgroups(
-        data_home=(DATADIR / "sklearn"),
+    SKDIR = common.DATADIR / "sklearn"
+    SKDIR.mkdir(exist_ok=True, parents=True)
+    bunch = sklearn.datasets.fetch_20newsgroups(
+        data_home=(SKDIR),
         subset="all",
         shuffle=False,
         random_state=get_seed(),
-        return_X_y=True,
         **kwargs
     )
-
-    return LabeledDataset.from_iterator(name, np.column_stack(labels, data))
+    labels = np.array([bunch.target_names[label] for label in bunch.target])
+    return LabeledDataset.from_iterator(
+        name,
+        np.column_stack([labels, bunch.data]),
+        overwrite=overwrite,
+    )
