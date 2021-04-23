41c134e760bd279ad926ebf6f967717f5d751dc5
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2021-04-23 18:00:56 +0200

    expended

--- tests/test_03_NMF.py
+++ tests/test_03_NMF.py
@@ -1,5 +1,6 @@
 import adso
-import adso.corpora as corpora
+from adso.corpora import get_20newsgroups
+from adso.algorithms import NMF
 
 
 def test_NMF():
@@ -8,9 +9,13 @@ def test_NMF():
     adso.set_project_name("test")
     adso.set_seed(8686)
 
-    dataset = corpora.get_20newsgroups(
-        "NMF_20news", categories=["sci.space", "rec.autos"]
+    dataset = get_20newsgroups(
+        "NMF_20news", overwrite=True, categories=["sci.space", "rec.autos"]
     )
+
+    nmf = NMF("test_NMF", 2, overwrite=True)
+
+    topic_model = nmf.fit_transform(dataset)
 
 
 if __name__ == "__main__":
