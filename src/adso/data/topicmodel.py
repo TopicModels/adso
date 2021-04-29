feca0f3c72261e7e8c74f757222d9333f3f7d6de
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2021-04-29 23:19:08 +0200

    get labels

--- src/adso/data/topicmodel.py
+++ src/adso/data/topicmodel.py
@@ -21,7 +21,29 @@ class TopicModel(Data):
             raise RuntimeError("Different hash")
 
     def get(self, skip_hash_check: bool = False) -> Tuple[da.array, da.array]:
-        return self.get_word_topic_matrix(), self.get_doc_topic_matrix()
+        return (
+            self.get_word_topic_matrix(),
+            self.get_doc_topic_matrix(),
+        )  # could give a concurrency error
+
+    def get_labels(self) -> da.array:
+        if self.hash != compute_hash(self.path):
+            raise RuntimeError("Different hash")
+        f = h5py.File(self.path, "a")  # work around for concurrent I/O
+        if "/labels" in f:
+            return da.from_array(f["/labels"])
+        else:
+            labels = da.argmax(da.from_array(f["/doc_topic"]), axis=1)
+            dset = f.create_dataset(
+                "/labels",
+                shape=labels.shape,
+                chunks=tuple([c[0] for c in labels.chunks]),  # from dask source to_hdf5
+                dtype=labels.dtype,
+            )
+            da.store(labels, dset)
+            self.update_hash()
+            f.close()
+            return da.from_array(h5py.File(self.path, "r")["/labels"])
 
     @classmethod
     def from_dask_array(
