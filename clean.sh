ba21c4a4822c982c2ab467a18be34b95ea6fc7ef
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2021-06-16 16:45:22 +0200

    clean docs

--- clean.sh
+++ clean.sh
@@ -1,1 +1,2 @@
 rm -rf .adso_test {,tests/}.test {,tests/}.ipynb_checkpoints .mypy_cache .nox .pytest_cache {,.}coverage coverage.xml {,tests/,src/adso/{,*/}}__pycache__ src/adso.egg-info
+./clean_docs.sh
---
+++ clean_docs.sh
@@ -0,0 +1,1 @@
+rm -rf docs/{_downloads,_examples,_images,_latex,_sources,_static,adso,*.html,objects.inv,*.js,*.pdf,.buildinfo,_source/_examples} 
