d11ed8c4ef64ddc3e26f6c8e79830ae55b357258
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2021-06-17 14:40:32 +0200

    fix

--- tests/test_11_wos.py
+++ tests/test_11_wos.py
@@ -1,5 +1,5 @@
 import adso
-from adso.corpora import get_wos46985
+from adso.corpora import get_wos
 
 
 def test_wos():
@@ -8,7 +8,7 @@ def test_wos():
     adso.set_project_name("test")
     adso.set_seed(8686)
 
-    get_wos46985("wos")
+    get_wos("wos")
 
 
 if __name__ == "__main__":
