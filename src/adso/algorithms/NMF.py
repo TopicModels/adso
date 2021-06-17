c522ad23692d0e171f88d7436c47a4516cb2242f
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2021-06-17 11:01:01 +0200

    lint

--- src/adso/algorithms/NMF.py
+++ src/adso/algorithms/NMF.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Tuple
 
 import sklearn.decomposition
-import sparse
 
 from ..common import get_seed
 from ..data.topicmodel import TopicModel
