78a2e5876c29fcbc0690809e82238287f21e93d0
Carlo Debernardi <carlo.debernardi.w@gmail.com>
2021-08-24 10:44:34 +0200

    Update UMAP_HDBSCAN.py

--- src/adso/algorithms/UMAP_HDBSCAN.py
+++ src/adso/algorithms/UMAP_HDBSCAN.py
@@ -15,14 +15,19 @@ class UMAP_HDBSCAN(TMAlgorithm):
         self.h_args = h_args
         self.kwargs = kwargs
 
-    def fit_transform(self, dataset: Dataset, name: str) -> TopicModel:
+    def fit_transform(self, dataset: Dataset, name: str, remove_disc = True) -> TopicModel:
+        # WARNING: setting a seed for reproducibility make the algorithm run on a single core (-> slower)
         seed = None
         if get_seed():
             seed = get_seed()
         # https://umap-learn.readthedocs.io/en/latest/index.html
         model = umap.UMAP(random_state = seed, **u_args).fit(dataset.get_count_matrix())
-        disc = umap.utils.disconnected_vertices(mapper)
-        embedding = mapper.embedding_[~disc,:]
+        # WARNING: some points might be disconnected (np.inf)
+        if remove_disc:
+            disc = umap.utils.disconnected_vertices(mapper)
+            embedding = mapper.embedding_[~disc,:]
+        else:
+            embedding = mapper.embedding_
         
         # https://hdbscan.readthedocs.io/en/latest/index.html
         clusterer = hdbscan.HDBSCAN(**h_args).fit(embedding)
